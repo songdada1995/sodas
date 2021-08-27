f014d5c10e77189ea538e6bbbcedb4e11cab07b1
songbo <1152745700@qq.com>
2021-08-27 22:57:30 +0800

    修改soda-server，增加配置中心、服务注册中心

---
+++ soda-server/src/main/java/com/sodas/sodaserver/config/controller/NacosConfigController.java
@@ -0,0 +1,22 @@
+package com.sodas.sodaserver.config.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RequestMapping("/config")
+@RestController
+public class NacosConfigController {
+
+    @Value("${app-name}")
+    private String appName;
+
+    @GetMapping("/get")
+    public String get() {
+        return "appName:" + appName;
+    }
+
+}
