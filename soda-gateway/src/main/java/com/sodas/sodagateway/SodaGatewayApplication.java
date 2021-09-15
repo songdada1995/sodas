7c5038e18199ca7e7f1773ecef21b19303262f02
songbo <1152745700@qq.com>
2021-09-15 23:26:23 +0800

    删除soda-server

--- soda-gateway/src/main/java/com/sodas/sodagateway/SodaGatewayApplication.java
+++ soda-gateway/src/main/java/com/sodas/sodagateway/SodaGatewayApplication.java
@@ -1,13 +1,16 @@
 package com.sodas.sodagateway;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class SodaGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SodaGatewayApplication.class, args);
+		log.info(">>>>> 网关模块启动成功 <<<<<");
 	}
 
 }
